6c436f0795d1a3073ee034aa951d457d48f8cff8
Pratik Garai <pratikgarai0208@gmail.com>
2020-07-28 11:09:43 +0530

    New high level implementation only

--- Main.py
+++ Main.py
@@ -11,7 +11,7 @@ def main():
         for j in range(API.mazeHeight()):
             API.setText(i,j,"0")
 
-    log("Running...")
+    log("Started")
     API.setColor(0, 0, "G")
     API.setText(0, 0, "Start")
     API.setColor(8, 8, "C")
@@ -23,13 +23,37 @@ def main():
     API.setColor(7, 7, "C")
     API.setText(7, 7, "Fin")
 
-    present_position = [0,0]
-    prev_position = [0,0]
+    direction = 0
+    present = [0,0]
+    past = [0,0]
 
     goals = [ [8,8], [8,7], [7,8], [7,7] ]
     directions = [ [0,1],[1,0],[0,-1],[-1,0] ]
 
     maze = [[ 0 for i in range(API.mazeHeight()) ] for j in range(API.mazeWidth())]
+
+    def get_openings(position):
+        pass
+
+    def move_to(past, future):
+        pass
+
+    def check(position):
+        if False:
+            sys.exit()
+        pass
+
+    def traverse(position):
+        check(position)
+        openings = get_openings(position)
+        if openings == [] :
+            return
+        for i in get_openings:
+            move_to(i)
+            traverse(i)
+            move_to(position)
+
+    log("Ended")
   
 if __name__ == "__main__":
     main()
