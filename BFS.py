ee88427df877eb5c5dd78086f6e4ef70d19103d6
PratikGarai <pratikgarai0208@gmail.com>
2020-11-20 13:32:33 +0530

    BFS ready to work

--- BFS.py
+++ BFS.py
@@ -1,38 +1,34 @@
 import pickle
 
-def ShortestPath(head): 
-	visited = [] 
-	
-	queue = [[head]] 
-	
-	while queue: 
-		path = queue.pop(0) #store the queue after pop first element
-		node = path[-1]  #store node to traverse
-		#print(node)
-	
-		if node not in visited: 
-			neighbours = node.neighbours #access neighbours of node
-			#print(neighbours)
+class Grapher():
 
-			for neighbour in neighbours: 
-				new_path = list(path) 
-				new_path.append(neighbour) 
-				#print(new_path)
-				queue.append(new_path)
-				#print(queue)
-				
-				
-				if neighbour.isEnd: 
-					print("Shortest path = ")
-					return new_path
-			
-			visited.append(node) 
+    def __init__(self, h):
+        self.root = h
 
-	print("Connecting path doesn't exist") 
-	return
+    def ShortestPath(self):
+        head = self.root
+        visited = [] 
+        queue = [[head]] 	
+        while queue: 
+            path = queue.pop(0)
+            node = path[-1]
+            if node not in visited: 
+                neighbours = node.neighbours
+                for neighbour in neighbours: 
+                    new_path = list(path) 
+                    new_path.append(neighbour) 
+                    queue.append(new_path)
+                    if neighbour.isEnd:
+                        print("Shortest path = ")
+                        return new_path
+                    visited.append(node)
+        print("Connecting path doesn't exist")
+        return
 
-
-if __name__ == "__main__": 
-	head = pickle.load(open("HeadNode.p", "rb"))
-
-        print(ShortestPath(head)) 
+if __name__ == "__main__":
+    head = pickle.load(open("HeadNode.p", "rb"))
+    g = Grapher(head)
+    path = g.ShortestPath()
+    print("Length : ", len(path))
+    for i in path :
+        print(i.pos, end = " , ")
